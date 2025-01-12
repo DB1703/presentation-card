5856aa7e5dc4a7a1888e7144834149fba57cb220
db1703 <debernardifranco2003@gmail.com>
2025-01-12 19:33:30 -0300

    Aplha layout with horizontal scroll

--- app/src/main/java/com/example/presentationcard/MainActivity.kt
+++ app/src/main/java/com/example/presentationcard/MainActivity.kt
@@ -4,13 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.presentationcard.ui.theme.PresentationCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +42,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PresentationCardTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MainContainer(
+                        fullName = "Android",
+                        shortDescription = "",
+                        //modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -31,17 +55,75 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FeatureCard (cardDescription: String, modifier: Modifier = Modifier) {
+    Box (modifier
+        .size(140.dp)
+        .background(color = Color(0xFF1F4E4B), shape = RoundedCornerShape(16.dp))
+        .padding(15.dp)
+    ) {
+        Column (verticalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxSize()){
+            Icon(painter = painterResource(R.drawable.comment_24px), contentDescription = "icon", tint = Color.White)
+            Text(text = cardDescription, modifier = modifier, color = Color.White)
+            Text(text = "Here we go two line", color = Color.White)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun FeatureCardsRow(modifier: Modifier = Modifier) {
+    val state = rememberScrollState()
+    Row(modifier.horizontalScroll(state).fillMaxSize()) {
+        FeatureCard("lorem ipsum")
+        Spacer(modifier = Modifier.width(25.dp))
+        FeatureCard("lorem ipsum")
+        Spacer(modifier = Modifier.width(25.dp))
+        FeatureCard("lorem ipsum")
+        Spacer(modifier = Modifier.width(25.dp))
+        FeatureCard("lorem ipsum")
+    }
+}
+
+@Composable
+fun CharacterIntroduction(fullName: String, shortDescription: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = fullName,
+            color = Color(0xFFB1D18A),
+            fontWeight = FontWeight.Bold,
+            fontSize = 50.sp,
+            textAlign = TextAlign.Center,
+            lineHeight = 60.sp,
+            modifier = modifier
+        )
+        Image(
+            painter = painterResource(R.drawable.swordsman),
+            contentDescription = "Person photograph",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(210.dp)
+                .clip(RoundedCornerShape(80.dp))
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        FeatureCardsRow()
+    }
+}
+
+@Composable
+fun MainContainer(fullName: String, shortDescription: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier.fillMaxSize().background(color = Color(0xFF12140E))
+    ) {
+        CharacterIntroduction("Lorem Ipsum", "Lorem ipsum sit amet dolor", modifier.padding(30.dp))
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     PresentationCardTheme {
-        Greeting("Android")
+        MainContainer(fullName = "Franco De Bernardi", shortDescription = "")
     }
 }
