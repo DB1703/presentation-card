b32ff7c07031b83749c42233100687721c70758b
db1703 <debernardifranco2003@gmail.com>
2025-01-12 20:01:57 -0300

    New icons for horizontal scroll

--- app/src/main/java/com/example/presentationcard/MainActivity.kt
+++ app/src/main/java/com/example/presentationcard/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -55,14 +56,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun FeatureCard (cardDescription: String, modifier: Modifier = Modifier) {
+fun FeatureCard (cardDescription: String, icon: Painter,modifier: Modifier = Modifier) {
     Box (modifier
         .size(140.dp)
         .background(color = Color(0xFF1F4E4B), shape = RoundedCornerShape(16.dp))
         .padding(15.dp)
     ) {
         Column (verticalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxSize()){
-            Icon(painter = painterResource(R.drawable.comment_24px), contentDescription = "icon", tint = Color.White)
+            Icon(painter = icon, contentDescription = "icon", tint = Color.White)
             Text(text = cardDescription, modifier = modifier, color = Color.White)
             Text(text = "Here we go two line", color = Color.White)
         }
@@ -73,13 +74,13 @@ fun FeatureCard (cardDescription: String, modifier: Modifier = Modifier) {
 fun FeatureCardsRow(modifier: Modifier = Modifier) {
     val state = rememberScrollState()
     Row(modifier.horizontalScroll(state).fillMaxSize()) {
-        FeatureCard("lorem ipsum")
+        FeatureCard("lorem ipsum", painterResource(R.drawable.comment_24px))
         Spacer(modifier = Modifier.width(25.dp))
-        FeatureCard("lorem ipsum")
+        FeatureCard("lorem ipsum", painterResource(R.drawable.forest_24px))
         Spacer(modifier = Modifier.width(25.dp))
-        FeatureCard("lorem ipsum")
+        FeatureCard("lorem ipsum", painterResource(R.drawable.rocket_launch_24px))
         Spacer(modifier = Modifier.width(25.dp))
-        FeatureCard("lorem ipsum")
+        FeatureCard("lorem ipsum", painterResource(R.drawable.cognition_24px))
     }
 }
 
